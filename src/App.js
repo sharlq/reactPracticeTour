6f9755085ae9641ca7bf19c27a767eb12c717f0a
sharlq <shehabakhader@gmail.com>
2021-04-10 13:45:53 +0300

    firts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState,useEffect} from 'react'
+import Header from "./components/header"
+import Tours from "./components/tours/tours"
 function App() {
+  const [toursList,setToursList] = useState([])
+  
+  const url = 'https://course-api.com/react-tours-project'
+  const fetchTours = async()=>{
+    try{
+      const response = await fetch(url)
+      const list =await response.json()
+      setToursList(list)
+    }catch(error){
+
+    }
+  }// shit i didnt knew how to do this
+  useEffect(()=>fetchTours(),[]);// shit wrote this wrong
+ /*useEffect((
+   setViewText()
+   ),[]);*/   // too many re-renders + how to sit it regardless it is not the right way
+  
+  console.log(toursList)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {/* <p>{toursList}</p> */}
+      <Tours toursList={toursList} />
     </div>
   );
 }
